c50a82cb36917fec47e02cc2a7b89049dcad710e
moteprem4-web <moteprem4@gmail.com>
2025-08-31 01:15:22 +0530

    added the resizing image file

---
+++ image resizing and shaping/resizing.py
@@ -0,0 +1,15 @@
+import cv2
+
+image=cv2.imread("image resizing and shaping/flower.jpg")
+
+if image is  not None:
+    print("the image is loaded")
+    resizing=cv2.resize(image,(300,300))
+    cv2.imshow("this is image",image)
+    cv2.imshow("resized",resizing)
+    cv2.waitKey(0) 
+    cv2.destroyAllWindows()
+
+
+else:
+    print("the image is not loaded")
